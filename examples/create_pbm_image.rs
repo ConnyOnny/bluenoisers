d9a6f6073c73cd9eda24bacc852af6c596579b3d
Constantin Berhard <git.mail.enormator@xoxy.net>
2016-07-28 17:38:06 +0200

    reduced example image size

--- examples/create_pbm_image.rs
+++ examples/create_pbm_image.rs
@@ -3,17 +3,17 @@ use bluenoisers::blue_noise;
 
 fn main() {
     // create the samples
-    let samples = blue_noise(vec![640.,480.], 4., 30);
+    let samples = blue_noise(vec![320.,240.], 4., 30);
     // create a boolean image to put the samples as pixels
-    let mut image = vec![vec![false; 640]; 480];
+    let mut image = vec![vec![false; 320]; 240];
     for s in samples {
         image[s[1] as usize][s[0] as usize] = true;
     }
     // write the image in portable bitmap format to stdout
     println!("P1");
-    println!("{} {}", 640, 480);
-    for y in 0..480 {
-        for x in 0..640 {
+    println!("{} {}", 320, 240);
+    for y in 0..240 {
+        for x in 0..320 {
             print!("{} ", if image[y][x] { "1" } else { "0" });
         }
         println!("");
